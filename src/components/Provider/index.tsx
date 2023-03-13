522f125fb94ab69cb0eaed5cbf3217775c79251a
Alexis Mineaud <amineaud@tenable.com>
2023-03-13 14:52:00 +0100

    Expose Provider.

--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -1,7 +1,8 @@
+import { Theme, themes } from 'src/styles/themes'
 import { ThemeProvider } from 'styled-components'
-import { themes } from 'src/styles/themes'
 
 export interface IProviderProps {
+  theme?: Theme
   children: React.ReactNode
 }
 
@@ -9,5 +10,7 @@ export interface IProviderProps {
  * Main provider to use in your app to make components working properly.
  */
 export default function Provider(props: IProviderProps) {
-  return <ThemeProvider theme={themes.default}>{props.children}</ThemeProvider>
+  const theme = props.theme ?? themes.default
+
+  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
-export { default as ContainerFlex } from './ContainerFlex'
+export { default as Provider } from './Provider'
 
+export { default as ContainerFlex } from './ContainerFlex'
 export type { IContainerFlexProps } from './ContainerFlex/types'
