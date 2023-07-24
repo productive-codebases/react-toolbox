594eb22cac77dab9bc8bebffe964cf6e1a6bf6a9
Alexis Mineaud <amineaud@tenable.com>
2023-07-24 15:20:29 +0200

    Convert eslint to JS + warn for unused vars.

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      tsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    camelcase: 'error',
+    'no-alert': 'error',
+    'no-console': 'error',
+    'no-duplicate-imports': 'error',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/display-name': 0,
+    'react/no-unescaped-entities ': 'off',
+    'react/no-unknown-property': 'off',
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    }
+  }
+}
